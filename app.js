b7ec68fd9b950f31efe397f1f75ae689d493ce38
vaibhavg4651 <vaibhavg4651@gmail.com>
2022-10-19 20:24:12 +0530

    EventEmitter in class

--- app.js
+++ app.js
@@ -1,6 +1,23 @@
-const _ = require('lodash')
+// const _ = require('lodash')
 
-const items = [1, [2, [3, [4]]]]
-const newItems = _.flattenDeep(items)
-console.log(newItems)
-console.log('hello people')
+// const items = [1, [2, [3, [4]]]]
+// const newItems = _.flattenDeep(items)
+// console.log(newItems)
+// console.log('hello people')
+
+
+// EventEmitter is a class
+const EventEmitter = require("events");
+const customEmitter = new EventEmitter()
+
+customEmitter.on('response',()=>{
+    console.log(`data received `);
+})
+
+customEmitter.on('response',()=>{
+    console.log(`some other logic here `);
+})
+
+
+
+customEmitter.emit('response')
